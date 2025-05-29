82d062559e006b33d53669c4fd86e77b89995786
Cinka <leofoxm40@gmail.com>
2025-05-29 21:52:26 +0300

    - add: sponsor implementation

---
+++ SponsorImplementations/Content.SponsorImplementations.Client/DependencyRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Content.Corvax.Interfaces.Shared;
+using Robust.Shared.IoC;
+
+namespace Content.SponsorImplementations.Client;
+
+internal static class DependencyRegistration
+{
+    public static void Register(IDependencyCollection dependencies)
+    {
+        dependencies.Register<ISharedSponsorsManager, ClientSponsorManager>();
+    }
+}
---
+++ SponsorImplementations/Content.SponsorImplementations.Server/ISponsorData.cs
@@ -0,0 +1,41 @@
+﻿using Robust.Shared.Maths;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.SponsorImplementations.Server;
+
+public interface ISponsorDataProvider
+{
+    public ISponsorData? GetSponsorInfo(NetUserId userId);
+}
+
+
+public interface ISponsorData
+{
+    public Guid Guid {get;}
+    public List<string> Prototypes {get;}
+    public Color? Color {get;}
+    public int ExtraCharSlots {get;}
+    public bool ServerPriorityJoin { get; }
+
+    public void CopyFrom(ISponsorData other);
+}
+
+[DataDefinition]
+public sealed partial class SponsorData: ISponsorData
+{
+    [DataField] public Guid Guid { get; set; }
+    [DataField] public List<string> Prototypes { get; set; }  = new();
+    [DataField] public Color? Color { get; set; }
+    [DataField] public int ExtraCharSlots { get; set; }
+    [DataField] public bool ServerPriorityJoin { get; set;}
+
+    public void CopyFrom(ISponsorData other)
+    {
+        Guid = other.Guid;
+        Prototypes = other.Prototypes;
+        Color = other.Color;
+        ExtraCharSlots = other.ExtraCharSlots;
+        ServerPriorityJoin = other.ServerPriorityJoin;
+    }
+}
