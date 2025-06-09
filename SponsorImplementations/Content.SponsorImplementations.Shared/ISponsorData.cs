8549efe1ddf9cbcf415662817fa62e32c95014ef
Cinka <leofoxm40@gmail.com>
2025-06-09 20:02:48 +0300

    - fix: YAMLLinter

--- SponsorImplementations/Content.SponsorImplementations.Shared/ISponsorData.cs
+++ SponsorImplementations/Content.SponsorImplementations.Shared/ISponsorData.cs
@@ -29,15 +29,3 @@ public sealed partial class SponsorData: ISponsorData
         ServerPriorityJoin = other.ServerPriorityJoin;
     }
 }
-
-[Prototype]
-public sealed partial class SponsorGroupPrototype : IPrototype, ISponsorData
-{
-    [IdDataField] public string ID { get; set; } = default!;
-
-    [DataField] public List<string> Prototypes { get; set;} = [];
-    [DataField] public Color? Color { get; set; }
-    [DataField] public int ExtraCharSlots { get; set;}
-    [DataField] public bool ServerPriorityJoin { get; set;}
-}
-
