026626ad8de1a21e75267d4a56385297ca3203a6
Qventydas <sulginanatala168@gmail.com>
2025-07-27 21:33:02 +0300

    Revert "Add system to kick people if they connect to multiple servers at once. (#34563)"
    This reverts commit ac89930b3914b1c88b65da5d40bfaa928676cbe1.

--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -75,9 +75,6 @@ namespace Content.Server.IoC
             IoCManager.Register<PlayerRateLimitManager>();
             IoCManager.Register<SharedPlayerRateLimitManager, PlayerRateLimitManager>();
             IoCManager.Register<MappingManager>();
-            IoCManager.Register<IWatchlistWebhookManager, WatchlistWebhookManager>();
-            IoCManager.Register<ConnectionManager>();
-            IoCManager.Register<MultiServerKickManager>();
         }
     }
 }
